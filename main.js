a771ab30837267656533bf0838ac3b87a67f2780
Chipsikus <bokmaks1@gmail.com>
2024-06-24 20:04:51 +0300

    js-main

--- main.js
+++ main.js
@@ -4,4 +4,43 @@ console.log('ЕСЛИ ВЫ ЧТО-ТО НАПИШИТЕ В КОНСОЛЬ ТО �
 console.log("БЛОКИРОВКАЙ ВАС НА САЙТЕ И КОПИРЫВАНИЯ ВАШЕЙ ЛИЧНОЙ ИНФОРМАЦИИ МОШЕННИКОВ"
 
 )
+let menu = document.querySelector(".fixed-menu");
 
+menu.addEventListener('click', function (event) {
+    let clickTarget = event.target;
+    let activeBtn = document.querySelector('.active');
+
+    if (clickTarget.classList.contains('nav-link')) {
+        clickTarget.classList.add('active');
+        activeBtn.classList.remove('active');
+    }
+});
+
+let classLink = '.main-link'
+let aboutLink = '.about-link'
+let skillaLink = 'works-link'
+
+window.onscroll = function () {
+    let h = document.documentElement.clientHeight;
+    if (window.scrollY >= h * 4) {
+        classLink = '.comments-link';
+    }
+    else if (window.scrollY >= h * 3) {
+        classLink = '.works-link';
+    }
+    else if (window.scrollY >= h * 2) {
+        classLink = '.skills-link';
+    }
+    else if (window.scrollY >= h) {
+        classLink = '.about-link';
+    }
+    else {
+        classLink = '.main-link';
+    }
+    let activeBtn = document.querySelector('.active');
+    let newActiveBtn = document.querySelector(classLink);
+    if (!newActiveBtn.classList.contains('active')) {
+        newActiveBtn.classList.add('active');
+        activeBtn.classList.remove('active')
+    }
+};
