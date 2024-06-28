c57826f295251f84e9b884f6426d6f73268a11d6
Chipsikus <bokmaks1@gmail.com>
2024-06-28 19:19:09 +0300

    lala

--- main.js
+++ main.js
@@ -44,3 +44,30 @@ window.onscroll = function () {
         activeBtn.classList.remove('active')
     }
 };
+
+document.querySelector(".mobile-button").addEventListener("click", function (event) {
+    document.querySelector(".mobile-menu").classList.toggle("hide");
+});
+
+menu.addEventListener('click', function (event) {
+    let clickTarget = event.target;
+    let activeBtn = document.querySelector('.active');
+    if (clickTarget.classList.contains('nav-link')) {
+        clickTarget.classList.add('active');
+        activeBtn.classList.remove('active'); 
+    }
+
+        let mobile = document.querySelector('.mobile-menu');
+        if (!mobile.classList.contains('hide')) {
+            mobile.classList.add('hide');
+        
+        }
+        let body = document.querySelector("body");
+        let mobileMenu = document.querySelector(".mobile-menu"); if (!body.classList.contains("off-scroll") &&
+            !mobileMenu.classList.contains('hide')) {
+            body.classList.add("off-scroll");
+            
+        }
+        else{ body.classList.remove("off-scroll");
+        }
+    });
